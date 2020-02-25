b88bd4f77a68669ec320d7c35b2640eefea7428c
gucktubeyt2 <61443077+gucktubeyt2@users.noreply.github.com>
2020-02-24 20:49:49 -0800

    Create server_data.php

---
+++ growtopia/server_data.php
@@ -0,0 +1,10 @@
+server|128.199.151.171
+port|44444
+type|1
+#maint|Mainetrance message (Not used for now) -- Growtopia Noobs
+
+beta_server|127.0.0.1
+beta_port|17091
+
+beta_type|1
+meta|localhostRTENDMARKERBS1001
